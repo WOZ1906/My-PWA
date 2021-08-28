ecf335ede7f14478bcbeb4061478891cf986effd
Tristian Pitogo <freaxwoz@gmail.com>
2021-08-29 01:41:30 +0800

    bug in line 4 sw.js corrected

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(caches => {
-            return cache.addAll(["./", "./style.css", "./images/Logo192.png"]);
+            return caches.addAll(["./", "./style.css", "./images/Logo192.png"]);
         })
     );
 });
