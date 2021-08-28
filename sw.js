4e8ecac1fc844051316cb67c4f5339ad796747ea
Tristian Pitogo <freaxwoz@gmail.com>
2021-08-29 01:14:47 +0800

    implementing sw

--- sw.js
+++ sw.js
@@ -6,4 +6,10 @@ self.addEventListener("install", e => {
     );
 });
 
-self.addEventListener
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    )
+})
